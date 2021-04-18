a3d6d4abf5a5007309788f460ac9c975f1f17cd9
sangwonyu <tkddnjs231@naver.com>
2021-04-18 09:16:02 +0900

    button animation

--- src/styles/indexStyle.ts
+++ src/styles/indexStyle.ts
@@ -1,31 +1,34 @@
 import styled from "styled-components";
 
-const Card = styled.a`
-  margin: 1rem;
-  flex-basis: 45%;
-  padding: 1.5rem;
-  text-align: left;
-  color: inherit;
-  text-decoration: none;
-  border: 1px solid #eaeaea;
-  border-radius: 10px;
-  transition: color 0.15s ease, border-color 0.15s ease;
-  &:hover,
-  &:focus,
-  &:active {
-    color: #0070f3;
-    border-color: #0070f3;
-  }
-  h3 {
-    margin: 0 0 1rem 0;
-    font-size: 1.5rem;
-  }
-  p {
-    margin: 0;
-    font-size: 1.25rem;
-    line-height: 1.5;
-  }
-`;
+// const Card = styled.a`
+//   margin: 1rem;
+//   flex-basis: 45%;
+//   padding: 1.5rem;
+//   text-align: left;
+//   color: inherit;
+//   text-decoration: none;
+//   border: 1px solid #eaeaea;
+//   border-radius: 10px;
+//   transition: color 0.15s ease, border-color 0.15s ease;
+//
+//   &:hover,
+//   &:focus,
+//   &:active {
+//     color: #0070f3;
+//     border-color: #0070f3;
+//   }
+//
+//   h3 {
+//     margin: 0 0 1rem 0;
+//     font-size: 1.5rem;
+//   }
+//
+//   p {
+//     margin: 0;
+//     font-size: 1.25rem;
+//     line-height: 1.5;
+//   }
+// `;
 
 const MainContainer = styled.div`
   flex-direction: column;
@@ -34,11 +37,11 @@ const MainContainer = styled.div`
 `;
 
 const MobileTitle = styled.div`
-    height: 40vh;
-    margin-top: 15vh;
-    font-size: 20px;
-    color: #222222;
-    font-family: 'Nunito', sans-serif;
+  height: 40vh;
+  margin-top: 15vh;
+  font-size: 20px;
+  color: #222222;
+  font-family: 'Nunito', sans-serif;
 `;
 
 const NextDiv = styled.div`
@@ -52,15 +55,26 @@ const NextDiv = styled.div`
 
 const KakaoLoginBtn = styled.button`
   background-color: #CECECE;
+  font-size: 14px;
+  font-family: 'Noto Sans KR', sans-serif;
   height: 33px;
   width: 56%;
   border-radius: 16px;
   border: none;
+  cursor: pointer;
+  outline: none;
+  &:hover {background-color: #FFD262}
+  &:active {
+    background-color: #FFD262;
+    box-shadow: 0 1px #666;
+    transform: translateY(3px);
+  }
 `;
 
 const GoMainBtn = styled.a`
-    padding-top: 18px;
-    text-decoration: none;
+  padding-top: 18px;
+  text-decoration: none;
+  cursor: pointer;
 `
 
-export {Card, MainContainer, MobileTitle, NextDiv, KakaoLoginBtn, GoMainBtn}
+export { MainContainer, MobileTitle, NextDiv, KakaoLoginBtn, GoMainBtn}
