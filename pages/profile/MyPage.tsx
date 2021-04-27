bdd063169a6ce357b331fd38cb3cc5854d107e12
juyeonji <jyj0216jyj@gmail.com>
2021-04-27 20:44:51 +0900

    add MyPage dir

---
+++ pages/profile/MyPage.tsx
@@ -0,0 +1,10 @@
+import {
+
+} from "../../src/styles/login/setNicnameStyle";
+import Link from "next/link";
+
+export default function SetNicName(): JSX.Element {
+    return (
+        <div>myPage</div>
+    );
+}
