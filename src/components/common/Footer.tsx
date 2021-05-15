9bbe926bf07f78a0a7b5c166aea8cc5f521bd1d9
sangwonyu <tkddnjs231@naver.com>
2021-05-15 17:34:57 +0900

    bubble 수정

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -34,7 +34,11 @@ const CreateStudy = (): JSX.Element => {
     }, 3000);
     return (
         <StudyCreateWrapper>
-            <BubbleImg src='/assets/main/bubble.png'/>
+            <CreateStudyBubble>
+                직접 스터디를 주최해보세요!
+            </CreateStudyBubble>
+            <BubblePointBackground/>
+            <BubblePoint/>
             <CookieIcon src='/assets/main/cookie.png'/>
             <Text>
                 스터디 만들기
@@ -111,6 +115,39 @@ const BubbleImg = styled.img`
   top: -89px;
   left: -46px;
   animation: ${boxFade} 1s 1s 1 linear forwards;
+`;
+
+const CreateStudyBubble = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #FFD262;
+  position: absolute;
+  border: 1px solid black;
+  width: 167px;
+  height: 30px;
+  top: -89px;
+  left: -47px;
+  font-size: 12px;
+  border-radius: 10px;
+
+`;
+
+const BubblePoint = styled.div`
+  position: absolute;
+  top: -58px;
+  border-top: 7px solid #FFD262;
+  border-left: 5px solid transparent;
+  border-right: 5px solid transparent;
+`;
+
+const BubblePointBackground = styled.div`
+  position: absolute;
+  top: -58px;
+  left: 32px;
+  border-top: 8px solid black;
+  border-left: 6px solid transparent;
+  border-right: 6px solid transparent;
 `;
 
 const Text = styled.div`
