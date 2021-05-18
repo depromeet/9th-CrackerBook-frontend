e31e5227ca69fddb11b921726efb9917aac0edec
sangwonyu <tkddnjs231@naver.com>
2021-05-19 07:01:49 +0900

    review box

--- src/components/detail/book/StudyReview/IndexComponent.tsx
+++ src/components/detail/book/StudyReview/IndexComponent.tsx
@@ -1,10 +1,12 @@
 import TitleComponent from "./TitleComponent";
+import ReviewBoxComponent from "./ReviewBoxComponent";
 
 export default function IndexComponent(): JSX.Element {
 
     return (
         <>
             <TitleComponent/>
+            <ReviewBoxComponent/>
         </>
     )
 }
--- src/components/detail/book/StudyReview/ReviewBoxComponent.tsx
+++ src/components/detail/book/StudyReview/ReviewBoxComponent.tsx
@@ -1,7 +1,93 @@
+import styled from "styled-components";
+
 export default function ReviewBoxComponent(): JSX.Element {
 
     return (
-        <>
-        </>
+        <BoxWrappers>
+            <BoxWrapper>
+                <IconWrapper>
+                    <ReviewIcon src='/assets/detail/best.svg'/>
+                    <div>
+                        최고에요
+                    </div>
+                </IconWrapper>
+                <Review>
+                    글 쓰는 과정에 대해 정말 즐겁게 배울 수 있었습니다. 책과 함께 글쓰는 과정이 더해져서 구체적
+                    으로 적용하기 좋았던 것 같아요.
+                    함께 읽으니까 너무 좋았어요 ...
+                </Review>
+                <ReviewerWrapper>
+                    <Profile src='/assets/detail/best.svg'/>
+                    <Wrapper>
+                        <Host>
+                            겁내지 않고 글 쓰는 법
+                        </Host>
+                        <Date>
+                            2021.04.26 ~ 05.21 토 14:00
+                        </Date>
+                    </Wrapper>
+                </ReviewerWrapper>
+            </BoxWrapper>
+        </BoxWrappers>
     )
 }
+const BoxWrappers = styled.div`
+  display: flex;
+  padding-bottom: 180px;
+`;
+
+const BoxWrapper = styled.div`
+  margin: 0 auto;
+  width: 225px;
+  height: 334px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 1px 2px 9px 0 #d4d3d3;
+`;
+
+const IconWrapper = styled.div`
+  padding-top: 32px;
+  padding-bottom: 20px;
+  text-align: center;
+`;
+
+const ReviewIcon = styled.img`
+  width: 52px;
+  height: 52px;
+`;
+
+const Review = styled.div`
+  padding: 0 20px 20px 32px;
+`;
+
+const ReviewerWrapper = styled.div`
+  padding: 14px 10px 14px 10px;
+  display: flex;
+  justify-content: left;
+  border-top: 1px solid #efefef;
+`;
+
+const Profile = styled.img`
+  border-radius: 50%;
+  padding-right: 10px;
+  width: 40px;
+  height: 40px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Host = styled.div`
+  font-weight: 500;
+  padding-bottom: 10px;
+  padding-top: 2px;
+  font-size: 13px;
+  color: #222222;
+`;
+
+const Date = styled.div`
+  font-size: 12px;
+  color: #777777;
+`;
