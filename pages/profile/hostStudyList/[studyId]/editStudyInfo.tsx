7225a1637ed961ae20c1b8a539b38ca9f9fc9e9c
juyeonji <jyj0216jyj@gmail.com>
2021-06-03 16:38:40 +0900

    add edit study

--- pages/profile/hostStudyList/[studyId]/editStudyInfo.tsx
+++ pages/profile/hostStudyList/[studyId]/editStudyInfo.tsx
@@ -1,4 +1,11 @@
 import React from "react";
+import StudyInfoComponent from "src/components/opening/StudyInfoComponent";
+import DifficultComponent from "src/components/opening/StudyInfoComponent/DifficultComponent";
+import IntroductionComponent from "src/components/opening/StudyInfoComponent/IntroductionComponent";
+import LocationComponent from "src/components/opening/StudyInfoComponent/LocationComponent";
+import NameComponent from "src/components/opening/StudyInfoComponent/NameComponent";
+import PersonnelComponent from "src/components/opening/StudyInfoComponent/PersonnelComponent";
+import StudyPeriodComponent from "src/components/opening/StudyPeriodComponent";
 
 export default function EditStudyInfo(): JSX.Element {
   const studyInfo = {
@@ -7,5 +14,9 @@ export default function EditStudyInfo(): JSX.Element {
     routingUrl: "profile/hostStudyList/admin/1",
     progressPercent: 15,
   };
-  return <div>스터디 수정</div>;
+  return <div>
+    {/*TODO: 분기 필요*/}
+    <StudyInfoComponent/>
+    <StudyPeriodComponent/>
+  </div>;
 }
