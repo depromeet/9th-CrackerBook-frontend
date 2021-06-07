62bcbd4bdff2939bb2dcd01f0e8ea2edc989aea2
juyeonji <jyj0216jyj@gmail.com>
2021-06-07 23:20:11 +0900

    add tabView animation

--- src/components/common/TabView.tsx
+++ src/components/common/TabView.tsx
@@ -6,6 +6,7 @@ const TabBar = styled.div`
   height: 62px;
   background: #fff;
   display: flex;
+  position: relative;
 `;
 
 const TabButton = styled.div`
@@ -16,10 +17,9 @@ const TabButton = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-
-  color: #999999;
+  transition: all 0.3s;
   &.on {
-    border-bottom: 2px solid #ffd262;
+    font-weight: 500;
     color: #222222;
   }
 `;
@@ -27,44 +27,55 @@ const TabContentsArea = styled.div`
   height: 100%;
   background: #f1f1f3;
   padding-top: 1vh;
-  padding-bottom: 1vh;
+`;
+
+const ActiveBar = styled.div<{ index: number; length: number }>`
+  position: absolute;
+  top: 60px;
+  height: 1px;
+  width: ${(props) => `${100 / props.length}%`};
+  left: ${(props) => `${(100 / props.length) * props.index}%`};
+  transition: all 0.3s;
+  border-bottom: 2px solid #ffd262;
 `;
 
 type tabInfo = {
-  name: string;
-  index: number;
+    name: string;
+    index: number;
 };
 
 export default function TabView(props: { tabInfo }): JSX.Element {
-  const tabList: tabInfo[] = [];
+    const tabList: tabInfo[] = [];
 
-  props.tabInfo.forEach(function (infoObj, index) {
-    tabList.push({ name: infoObj.tabName, index: index });
-  });
+    props.tabInfo.forEach(function (infoObj, index) {
+        tabList.push({ name: infoObj.tabName, index: index });
+    });
 
-  const [tabState, setTabSTate] = React.useState(0);
+    const [tabState, setTabSTate] = React.useState(0);
 
-  const clickHandler = (index) => {
-    setTabSTate(index);
-  };
+    const clickHandler = (index) => {
+        console.log(tabState);
+        setTabSTate(index);
+    };
 
-  return (
-    <div style={{ height: "100%" }}>
-      <TabBar>
-        {tabList.map((tab, index) => {
-          return (
-            <TabButton
-              style={{ width: 100 / tabList.length + "%" }}
-              key={tab.index}
-              onClick={() => clickHandler(index)}
-              className={tabState === index ? "on" : ""}
-            >
-              <div>{tab.name}</div>
-            </TabButton>
-          );
-        })}
-      </TabBar>
-      <TabContentsArea>{props.tabInfo[tabState].tabContents}</TabContentsArea>
-    </div>
-  );
+    return (
+        <>
+            <TabBar>
+                {tabList.map((tab, index) => {
+                    return (
+                        <TabButton
+                            style={{ width: 100 / tabList.length + "%" }}
+                            key={tab.index}
+                            onClick={() => clickHandler(index)}
+                            className={tabState === index ? "on" : ""}
+                        >
+                            <div>{tab.name}</div>
+                        </TabButton>
+                    );
+                })}
+                <ActiveBar index={tabState} length={tabList.length} />
+            </TabBar>
+            <TabContentsArea>{props.tabInfo[tabState].tabContents}</TabContentsArea>
+        </>
+    );
 }
