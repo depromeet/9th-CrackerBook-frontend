7412efd7fe472afb03ad1903f3f8db64c988b4ef
Juyeon Ji <46771574+Juyeon-Ji@users.noreply.github.com>
2021-06-12 05:19:42 +0900

    commit changes

--- pages/profile/hostStudyList/index.tsx
+++ pages/profile/hostStudyList/index.tsx
@@ -5,24 +5,47 @@ import Head from "next/head";
 import HeaderBar from "src/components/common/HeaderBar";
 
 export default function InterestedBook(): JSX.Element {
-  const studyInfo = {
-    title: "사용자의 마음을 움직이는 UX 디자인의 힘",
-    type: "토론",
-    routingUrl: "profile/hostStudyList/1",
-    progressPercent: 15,
-    isLike: true,
-  };
+  const studyInfo = [
+    {
+      imgUrl: "/assets/mock/Book15.svg",
+      title: "데이터 분석하는 디자이너 되기",
+      type: "글쓰기",
+      routingUrl: "detail/study/studyDetail",
+      progressPercent: 10,
+    },
+    {
+      imgUrl: "/assets/mock/Book16.svg",
+      title: "IT 서비스 마케터 모임",
+      type: "글쓰기",
+      routingUrl: "detail/study/studyDetail",
+      progressPercent: 20,
+    },
+
+    {
+      imgUrl: "/assets/mock/Book17.svg",
+      title: "비전공자를 위한 IT 지식",
+      type: "토론",
+      routingUrl: "detail/study/studyDetail",
+      progressPercent: 50,
+    },
+  ];
 
   const bookSize = { height: 137, width: 95 };
 
   return (
     <div>
       <HeaderBar title={"주최한 스터디"} routingUrl={"profile"} />
-      <div>
-        <HorizontalStudy isOver={true} size={bookSize} studyInfo={studyInfo} />
-        <HorizontalStudy isOver={true} size={bookSize} studyInfo={studyInfo} />
-        <HorizontalStudy isOver={true} size={bookSize} studyInfo={studyInfo} />
-        <HorizontalStudy isOver={true} size={bookSize} studyInfo={studyInfo} />
+      <div style={{ height: "100%", padding: "20px" }}>
+        {studyInfo.map((data, index) => {
+          return (
+            <HorizontalStudy
+              key={index}
+              isOver={true}
+              size={bookSize}
+              studyInfo={data}
+            />
+          );
+        })}
       </div>
     </div>
   );
